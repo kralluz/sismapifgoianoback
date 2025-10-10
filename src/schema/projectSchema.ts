bf1e087ec7ede43025d3a6ee8b09fccf4f917557
kralluz <chenrique201520@gmail.com>
2025-10-10 20:33:29 -0300

    Update projectSchema.ts

--- src/schema/projectSchema.ts
+++ src/schema/projectSchema.ts
@@ -2,7 +2,7 @@ import {z} from 'zod';
 
 export const projectSchema = z.object({
   number: z.number().min(1),
-  title: z.string().min(2).max(100),
+  title: z.string().min(2).max(1000),
   roomId: z.number().min(1),
 });
 
